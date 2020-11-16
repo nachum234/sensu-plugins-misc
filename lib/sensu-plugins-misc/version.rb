9677c70ce543210eae65e965e0c75a28a85d5bcb
Yossi Nachum <yossi.nachum@biscience.com>
2020-11-16 14:07:58 +0200

    add dependencies

---
+++ lib/sensu-plugins-misc/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module SensuPluginsMisc
+  module Version
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 3
+
+    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+  end
+end
